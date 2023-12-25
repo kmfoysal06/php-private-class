3e61146f1a1b814b19c6926522ad5f0c866e0569
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-12-25 16:46:45 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP File Uploading</title>
+</head>
+<body>
+	<form method="post" enctype="multipart/form-data">
+		<input type="file" name="image" id="image">
+		<input type="submit" name="submit" value="submit">
+	</form>
+</body>
+</html>
