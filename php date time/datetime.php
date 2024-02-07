9f212bbef44bdc3eddfb51498acfd2bb482663fb
Kazi Mohammad Foysal <foysalkazimd01@gmail.com>
2024-02-07 16:12:27 +0600

    updated

--- php date time/datetime.php
+++ php date time/datetime.php
@@ -7,9 +7,22 @@
 // $output = date('D',$date);
 // $output = date('d',strtotime('next sunday'));
 
+date_default_timezone_set('Asia/Dhaka');
+// $output = date('D - d - M - Y h:i:s a');
+
+// $output = date("D-M-Y",mktime(8,20,54,12,03,2006));
+    // $startDate = strtotime('last friday');
+    // $endDate = strtotime('+1 week');
+
+// $output = date('d-m-Y',$endDate);
+// while($startDate < $endDate){
+//     $remainDay += 1;
+//     $startDate = strtotime('+1 day',$startDate);
+// }
+// echo 'Total Days: '.$remainDay;
+// echo var_dump(date('d-m-Y',strtotime('today')));
 
 
-
-
-
-echo var_dump($output);
+// $endDate = strtotime('next tuesday');
+// $remainDay = $endDate - strtotime('today');
+// echo $remainDay/(60*60*24).' Day remain to next sunday'; 
