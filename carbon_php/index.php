84f0a41a550287f3322cb8a208dd951dea9c1a33
Kazi Mohammad Foysal <foysalkazimd01@gmail.com>
2024-02-13 18:16:52 +0600

    updated

--- carbon_php/index.php
+++ carbon_php/index.php
@@ -2,6 +2,12 @@
 require_once "vendor/autoload.php";
 use Carbon\Carbon;
 
-$c = new Carbon;
+// $c = new Carbon;
 // echo var_dump($c.timezone);
-echo Carbon::now();
+// echo Carbon::now();
+// $c = Carbon::create(2012,03,12,12,10,40);
+// $c = Carbon::createFromDate(2012,03,12);
+// $c = Carbon::createFromTimestamp(1142121600);
+// $c = strtotime('2006-03-12');
+$c = new Carbon('6th March 2006 12:10:40', 'asia/dhaka');
+echo var_dump($c);
