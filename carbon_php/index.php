bfabe03b25b3dfb33dbb8ed786daf8772088b126
Kazi Mohammad Foysal <foysalkazimd01@gmail.com>
2024-02-16 16:30:46 +0600

    updated

--- carbon_php/index.php
+++ carbon_php/index.php
@@ -14,7 +14,9 @@ use Carbon\Carbon;
 // $c = new Carbon('+2 year 5 days', 'asia/dhaka');
 
 
-$c = Carbon::now();
+// $c = Carbon::now();
+// $c = new Carbon('12/3/2006');
+// echo $c->format('l jS F Y h:i:s');
 //print the date
 // echo var_dump($c->toDateString());
 //print the time
@@ -24,4 +26,22 @@ $c = Carbon::now();
 // print the rss string
 // echo $c->toRssString();
 // print in atom string
-echo $c->toAtomString();
+// echo $c->toAtomString();
+
+// carbon custom time formate
+// echo $c->format('l jS F Y h:i:s');
+
+// monday 12th march 2012 12:10:40
+
+// echo $c->format('l jS F Y h:i:s');
+// $diff = $c->diffForHumans();
+
+$c = new Carbon('-2 years 5 days','asia/dhaka');
+// echo var_dump($c->year);
+// echo var_dump($c->month);
+// echo $c->tz;
+// $c->addYears(5);
+// $c->subYears(4);
+// echo $c->toDateTimeString();
+
+$start
