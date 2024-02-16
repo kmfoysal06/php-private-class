ce76b3dd1420f500ad7785b1c3cfd5e0991dda11
Kazi Mohammad Foysal <foysalkazimd01@gmail.com>
2024-02-16 15:56:13 +0600

    updated

--- carbon_php/index.php
+++ carbon_php/index.php
@@ -11,5 +11,17 @@ use Carbon\Carbon;
 // $c = strtotime('2006-03-12');
 // $c = new Carbon('6th March 2006 12:10:40', 'asia/dhaka');
 // $c = new Carbon('+2 year 5 days', 'asia/dhaka');
-$c = new Carbon('+2 year 5 days', 'asia/dhaka');
-echo var_dump($c);
+// $c = new Carbon('+2 year 5 days', 'asia/dhaka');
+
+
+$c = Carbon::now();
+//print the date
+// echo var_dump($c->toDateString());
+//print the time
+// echo $c->toDateTimeString();
+
+
+// print the rss string
+// echo $c->toRssString();
+// print in atom string
+echo $c->toAtomString();
