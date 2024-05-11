9026a87c51950755697fd4f39954ba83e5ef719d
Kazi Mohammad Foysal <foysalkazimd01@gmail.com>
2024-05-11 18:31:02 +0600

    added interface

--- php oop/advanched/oop.php
+++ php oop/advanched/oop.php
@@ -6,4 +6,36 @@ require_once "vendor/autoload.php";
 $user = new Users;
 $user->setUserData('foysal',9,'male');
 $vip =  new VIP_User();
-echo var_dump($vip->age_validate($user->get_user_age()));
+// echo var_dump($vip->age_validate($user->get_user_age()));
+
+// interface Animal{
+// 	public function makeSound();
+// 	// public function makeSound2();
+// }
+// class Cat implements Animal{
+// 	public function makeSound(){
+// 		echo "Meow";
+// 	}
+// }
+// $cat = new Cat;
+// $cat->makeSound();
+interface payment{
+	public function pay($amount);
+}
+class paypal implements payment{
+	public function pay($amount){
+		echo $amount;
+	}
+}
+class stripe implements payment{
+	public function pay($amount){
+		echo $amount;
+	}
+}
+class nagad implements payment{
+	public function pay($amount){
+		echo $amount;
+	}
+}
+$paypal = new paypal;
+$paypal->pay(10);
